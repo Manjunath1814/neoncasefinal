9b012dd0feb567e939ee3c4b8385347b83f5cba4
Manjunath1814 <manuvlogs982@gmail.com>
2026-08-14 22:08:35 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,26 +1,26 @@
-import {
-    initializeApp
-} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+// firebase-config.js
 
-import {
-    getFirestore
-} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+export const firebaseConfig = {
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBleFUWJq9xiSRZCMzxZG9VmWDayBmcGTg",
-    authDomain: "neoncase.firebaseapp.com",
-    projectId: "neoncase",
-    storageBucket: "neoncase.firebasestorage.app",
-    messagingSenderId: "1004163074284",
-    appId: "1:1004163074284:web:4997dca90a0f06346bbdc9",
-    measurementId: "G-NPMZHKJLYH"
-};
+    apiKey:
+        "AIzaSyBleFUWJq9xiSRZCMzxZG9VmWDayBmcGTg",
 
-const app = initializeApp(firebaseConfig);
+    authDomain:
+        "neoncase.firebaseapp.com",
 
-const db = getFirestore(app);
+    projectId:
+        "neoncase",
 
-export {
-    app,
-    db
+    storageBucket:
+        "neoncase.firebasestorage.app",
+
+    messagingSenderId:
+        "1004163074284",
+
+    appId:
+        "1:1004163074284:web:4997dca90a0f06346bbdc9",
+
+    measurementId:
+        "G-NPMZHKJLYH"
+
 };
