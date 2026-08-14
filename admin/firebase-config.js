08c42b3a910d18c35d2b2c019d87f4fb05f45630
Manjunath1814 <manuvlogs982@gmail.com>
2026-08-14 18:54:19 +0530

    Update firebase-config.js

--- admin/firebase-config.js
+++ admin/firebase-config.js
@@ -1,26 +1,24 @@
-// firebase-config.js
+import {
+    initializeApp
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
-export const firebaseConfig = {
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
-    apiKey:
-        "AIzaSyBleFUWJq9xiSRZCMzxZG9VmWDayBmcGTg",
-
-    authDomain:
-        "neoncase.firebaseapp.com",
-
-    projectId:
-        "neoncase",
-
-    storageBucket:
-        "neoncase.firebasestorage.app",
-
-    messagingSenderId:
-        "1004163074284",
-
-    appId:
-        "1:1004163074284:web:4997dca90a0f06346bbdc9",
-
-    measurementId:
-        "G-NPMZHKJLYH"
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBleFUWJq9xiSRZCMzxZG9VmWDayBmcGTg",
+  authDomain: "neoncase.firebaseapp.com",
+  projectId: "neoncase",
+  storageBucket: "neoncase.firebasestorage.app",
+  messagingSenderId: "1004163074284",
+  appId: "1:1004163074284:web:4997dca90a0f06346bbdc9",
+  measurementId: "G-NPMZHKJLYH"
 };
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
